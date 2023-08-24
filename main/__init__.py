e801a1c92e24be64392628b3b19db1b7e5eaa798
ThechauhanMahesh <125787905+ThechauhanMahesh@users.noreply.github.com>
2023-08-24 15:21:24 +0530

    Update __init__.py

--- main/__init__.py
+++ main/__init__.py
@@ -12,5 +12,6 @@ logging.basicConfig(format='[%(levelname) 5s/%(asctime)s] %(name)s: %(message)s'
 API_ID = 2992000
 API_HASH = "235b12e862d71234ea222082052822fd"
 BOT_TOKEN = "5003282285:AAEHj1my8wS1X7kF6hzOFOq0ivDqbFBROpk"
-
+AUTH = [5351121397]
+MONGODB_URI = "mongodb+srv://Vasusen:darkmaahi@cluster0.o7uqb.mongodb.net/cluster0?retryWrites=true&w=majority"
 bot = TelegramClient('bot', API_ID, API_HASH).start(bot_token=BOT_TOKEN) 
